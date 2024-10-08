c2fed4624cce8087c196de9d963c793b9c1dbf65
Yossy Adirta <yossyadirta@gmail.com>
2024-10-08 22:21:17 +0700

    Init setup project

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--plus-jakarta-sans",
+});
+
+export const metadata: Metadata = {
+  title: "JanjiSehat",
+  description: "A Healthcare Appointment App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-light-200 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
